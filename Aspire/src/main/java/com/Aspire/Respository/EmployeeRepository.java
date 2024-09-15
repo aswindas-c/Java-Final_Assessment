a8a94c30ac6728f6a7c04aee28724879c2098b31
Aswindas Chemban(UST <285151@ust.com>
2024-09-15 17:56:51 +0530

    Basic post

--- Aspire/src/main/java/com/Aspire/Respository/EmployeeRepository.java
+++ Aspire/src/main/java/com/Aspire/Respository/EmployeeRepository.java
@@ -1,22 +1,16 @@
 package com.Aspire.Respository;
 
 
-import java.time.Instant;
 import java.util.List;
 
 import com.Aspire.model.Employee;
 
 
 public interface EmployeeRepository {
-    List<Employee> findByManagerIdAndDateOfJoiningBefore(Integer managerId, Instant minJoiningDate);
-    List<Employee> findByManagerId(Integer managerId);
-    List<Employee> findByDateOfJoiningBefore(Instant minJoiningDate);
     List<Employee> findByStream(String stream);
     Integer findMaxId();
-    Employee save(Employee Employee);
+    Employee save(Employee employee);
     Employee insert(Employee Employee);
-    void delete(Employee Employee);
     boolean existsById(Integer id);
     Employee findById(Integer id);
-    List<Employee> findAll();
 }
