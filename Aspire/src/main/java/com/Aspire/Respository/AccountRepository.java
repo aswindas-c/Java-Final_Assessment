8d401170c0fcc6c4e7cd580dde233dfd0c802d28
Aswindas Chemban(UST <285151@ust.com>
2024-09-19 09:30:54 +0530

    Database changed to mysql

--- Aspire/src/main/java/com/Aspire/Respository/AccountRepository.java
+++ Aspire/src/main/java/com/Aspire/Respository/AccountRepository.java
@@ -1,7 +1,9 @@
 package com.Aspire.Respository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.Aspire.model.Account;
 
-public interface AccountRepository {
+public interface AccountRepository extends JpaRepository<Account,String>{
     Account findByName(String name);
 }
--- Aspire/src/main/java/com/Aspire/Respository/ManagerRepository.java
+++ Aspire/src/main/java/com/Aspire/Respository/ManagerRepository.java
@@ -1,10 +1,13 @@
 package com.Aspire.Respository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.Aspire.model.Manager;
 
-public interface ManagerRepository {
-    Manager save(Manager manager);
-    Manager insert(Manager manager);
-    Manager findById(Integer id);
-    void delete(Manager manager);
+
+public interface ManagerRepository extends JpaRepository<Manager,Integer>{
+
+    @Query("SELECT e FROM Manager e WHERE e.id = :id")
+    Manager findUsingid(Integer id);
 }
