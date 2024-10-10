4e05c5c9b2b8c4967caeb9b0640ea07b177dec9e
Aswindas Chemban(UST <285151@ust.com>
2024-10-10 10:00:20 +0530

    Tests updated

--- Inventory-Management/src/test/java/com/example/Inventory_Management/ProductServiceTest.java
+++ Inventory-Management/src/test/java/com/example/Inventory_Management/ProductServiceTest.java
@@ -52,28 +52,21 @@ class ProductServiceTest {
     @Test
     void testAddProduct_Success() {
         ProductDto productDto = new ProductDto();
-        productDto.setName("TestProduct");
+        productDto.setName("Car");
         productDto.setCategoryId(1);
         productDto.setPrice(100.0);
         productDto.setQuantity(10);
 
         Category category = new Category();
         category.setId(1);
-        category.setName("TestCategory");
-
-        Product savedProduct = new Product();
-        savedProduct.setId(1);  // Simulate auto-generated ID
-        savedProduct.setName("TestProduct");
-        savedProduct.setCategory(category);
-        savedProduct.setPrice(100.0);
-        savedProduct.setQuantity(10);
+        category.setName("Vehicle");
 
         when(categoryRepo.findById(1)).thenReturn(Optional.of(category));
-        when(productRepo.findByName("TestProduct")).thenReturn(null);
+        when(productRepo.findByName("Car")).thenReturn(null);
         
         when(productRepo.save(any(Product.class))).thenAnswer(invocation -> {
             Product product = invocation.getArgument(0);
-            product.setId(1);  // Simulate the ID being set after save
+            product.setId(1); 
             return product;
         });
         Response response = productService.addProduct(productDto);
@@ -87,24 +80,18 @@ class ProductServiceTest {
     void testAddProduct_ProductNameExists() {
         // Setup
         ProductDto productDto = new ProductDto();
-        productDto.setName("TestProduct");
+        productDto.setName("Car");
         productDto.setCategoryId(1);
         productDto.setPrice(100.0);
         productDto.setQuantity(10);
 
         Category category = new Category();
         category.setId(1);
-        category.setName("TestCategory");
+        category.setName("Vehicle");
 
-        Product savedProduct = new Product();
-        savedProduct.setId(1);  // Simulate auto-generated ID
-        savedProduct.setName("TestProduct");
-        savedProduct.setCategory(category);
-        savedProduct.setPrice(100.0);
-        savedProduct.setQuantity(10);
 
         when(categoryRepo.findById(1)).thenReturn(Optional.of(category));
-        when(productRepo.findByName("TestProduct")).thenReturn(savedProduct);
+        when(productRepo.findByName("Car")).thenReturn(productDto);
 
         Exception exception = assertThrows(KeyAlreadyExistsException.class, () -> {
             productService.addProduct(productDto);
@@ -113,7 +100,7 @@ class ProductServiceTest {
         assertEquals("Product name already exists.", exception.getMessage());
     }
 
-    //add product - quantity neagtive
+    //add product - quantity negative
     @Test
     void testAddProduct_StockLevelNegative() {
         ProductDto productDto = new ProductDto();
